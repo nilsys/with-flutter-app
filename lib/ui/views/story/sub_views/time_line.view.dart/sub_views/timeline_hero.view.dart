bc604fb5c51ffe27e70888e7da158cc57f88a984
ofersarid <ofersarid@gmail.com>
2020-09-13 11:56:30 +0300

    story hero section main logic completed missing design

--- lib/ui/views/story/sub_views/time_line.view.dart/sub_views/timeline_hero.view.dart
+++ lib/ui/views/story/sub_views/time_line.view.dart/sub_views/timeline_hero.view.dart
@@ -99,7 +99,7 @@ class _TimelineHeroState extends State<TimelineHero> {
               max(constraints.biggest.height - _paddingTop - _appBarHeight, 0);
           final squeeze = _height / (expandedHeight - _appBarHeight);
           return Container(
-            color: Colors.green,
+            // color: Colors.green,
             height: double.infinity,
             child: Opacity(
               opacity: 1 * squeeze,
@@ -108,15 +108,49 @@ class _TimelineHeroState extends State<TimelineHero> {
                 alignment: Alignment.bottomCenter,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Avatar(
-                      src: widget.user.profileImage,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 15.0),
-                      child: Text(widget.user.displayName),
-                    ),
-                  ],
+                  children: widget.user != null
+                      ? [
+                          Avatar(
+                            src: widget.user.profileImage,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 15.0),
+                            child: Text(widget.user.displayName),
+                          ),
+                        ]
+                      : [
+                          SkeletonLoader(
+                            builder: Column(
+                              children: [
+                                Container(
+                                  width: 50.0,
+                                  height: 50.0,
+                                  margin: EdgeInsets.only(bottom: 5.0),
+                                  decoration: new BoxDecoration(
+                                    color: Colors.black,
+                                    shape: BoxShape.circle,
+                                  ),
+                                ),
+                                Container(
+                                  color: Colors.black,
+                                  width: 160.0,
+                                  height: 18.0,
+                                  margin: EdgeInsets.only(bottom: 15.0),
+                                ),
+                              ],
+                            ),
+                            items: 1,
+                            period: Duration(seconds: 2),
+                            baseColor:
+                                TinyColor(Theme.of(context).primaryColorLight)
+                                    .lighten(4)
+                                    .color,
+                            hightlightColor:
+                                TinyColor(Theme.of(context).primaryColorLight)
+                                    .lighten(10)
+                                    .color,
+                          ),
+                        ],
                 ),
               ),
             ),
